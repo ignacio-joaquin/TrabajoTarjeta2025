963e8b09daf3a6b40e2feda4ef2592a7cc6e7d0b
nasho <i.joaquinbertone@gmail.com>
2025-10-28 15:38:19 -0300

    comitie un coso vacio que gaga

---
+++ Tarjeta/FranquiciaCompleta.cs
@@ -0,0 +1,20 @@
+namespace TarjetaSube
+{
+    public class FranquiciaCompleta : Tarjeta
+    {
+        public FranquiciaCompleta(int saldoInicial = 0) : base(saldoInicial)
+        {
+        }
+
+        public override bool Descontar(int monto)
+        {
+            // Viaje gratuito - siempre permite el viaje
+            return true;
+        }
+
+        public override int CalcularMontoPasaje(int tarifaBase)
+        {
+            return 0;
+        }
+    }
+}
