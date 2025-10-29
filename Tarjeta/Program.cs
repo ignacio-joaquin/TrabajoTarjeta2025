a44b18eb3a078c970d062a7db8ef7915187a6e37
nasho <i.joaquinbertone@gmail.com>
2025-10-29 11:15:25 -0300

    no se podia

---
+++ Tarjeta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tarjeta
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guau, cuanto vacio\n");
+        }
+    }
+}
